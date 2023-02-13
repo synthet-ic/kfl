86bc79742ada90bb9d56db6b351549d882388dbf
Rin Arakaki <rnarkkx@gmail.com>
2023-02-13 14:19:47 +0900

    Create scalar.rs

---
+++ derive/tests/scalar.rs
@@ -0,0 +1,43 @@
+use std::fmt;
+
+use kfl::{
+    Decode, DecodeScalar,
+    span::Span
+};
+use miette::Diagnostic;
+
+
+#[derive(DecodeScalar, Debug, PartialEq)]
+enum SomeScalar {
+    First,
+    AnotherOption,
+}
+
+#[derive(Decode, Debug, PartialEq)]
+struct Item {
+    #[knuffel(argument)]
+    value: SomeScalar,
+}
+
+fn parse<T: Decode<Span>>(text: &str) -> T {
+    let mut nodes: Vec<T> = knuffel::parse("<test>", text).unwrap();
+    assert_eq!(nodes.len(), 1);
+    nodes.remove(0)
+}
+
+fn parse_err<T: Decode<Span>+fmt::Debug>(text: &str) -> String {
+    let err = knuffel::parse::<Vec<T>>("<test>", text).unwrap_err();
+    err.related().unwrap()
+        .map(|e| e.to_string()).collect::<Vec<_>>()
+        .join("\n")
+}
+
+#[test]
+fn parse_some_scalar() {
+    assert_eq!(parse::<Item>(r#"node "first""#),
+               Item { value: SomeScalar::First } );
+    assert_eq!(parse::<Item>(r#"node "another-option""#),
+               Item { value: SomeScalar::AnotherOption } );
+    assert_eq!(parse_err::<Item>(r#"node "test""#),
+        "expected one of `first`, `another-option`");
+}

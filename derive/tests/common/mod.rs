70836a4df4becb3bb23249388ed841d91a2b131a
Rin Arakaki <rnarkkx@gmail.com>
2023-02-15 19:09:08 +0900

    Update mod.rs

--- derive/tests/common/mod.rs
+++ derive/tests/common/mod.rs
@@ -11,7 +11,7 @@ pub fn parse<T: Decode<Span>>(input: &str) -> T {
     nodes.remove(0)
 }
 
-fn hint_same_type<T: PartialEq>(lhs: &T, rhs: &T) {}
+pub fn hint_same_type<T: PartialEq>(lhs: &T, rhs: &T) {}
 
 #[macro_export]
 macro_rules! assert_parse {

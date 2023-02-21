06801e640b3f343657e4a73a23e35b185a8a1cc0
root <rnarkkx@gmail.com>
2023-02-21 11:25:28 +0000

    Update

--- src/span.rs
+++ src/span.rs
@@ -17,7 +17,7 @@ use std::{
     ops::Range
 };
 
-use crate::traits;
+// use crate::traits;
 
 /// Reexport of [miette::SourceSpan] trait that we use for parsing
 pub use miette::SourceSpan as ErrorSpan;
@@ -58,27 +58,22 @@ pub struct LineSpan(
     pub LinePos,
 );
 
-#[allow(missing_debug_implementations)]
-mod sealed {
-
-    pub struct OffsetTracker {
-        pub(crate) offset: usize,
-    }
-
-    #[cfg(feature="line-numbers")]
-    pub struct LineTracker {
-        pub(crate) offset: usize,
-        pub(crate) caret_return: bool,
-        pub(crate) line: usize,
-        pub(crate) column: usize,
-    }
-
-}
-
 impl Span {
     /// Length of the span in bytes
-    pub fn length(&self) -> usize {
+    pub fn len(&self) -> usize {
         self.1.saturating_sub(self.0)
+    }
+    ///
+    pub fn at_start(&self, chars: usize) -> Self {
+        Span(self.0, self.0 + chars)
+    }
+    ///
+    pub fn at_end(&self) -> Self {
+        Span(self.1, self.1)
+    }
+    ///
+    pub fn before_start(&self, chars: usize) -> Self {
+        Span(self.0.saturating_sub(chars), self.0)
     }
 }
 
@@ -88,180 +83,191 @@ impl Into<ErrorSpan> for Span {
     }
 }
 
+impl From<chumsky::zero_copy::span::SimpleSpan<usize>> for Span {
+    fn from(value: chumsky::zero_copy::span::SimpleSpan<usize>) -> Self {
+        Self(value.start, value.end)
+    }
+}
+
 impl Into<ErrorSpan> for LineSpan {
     fn into(self) -> ErrorSpan {
         (self.0.offset, self.1.offset.saturating_sub(self.0.offset)).into()
     }
 }
 
-impl chumsky::Span for Span {
+impl chumsky::zero_copy::span::Span for Span {
     type Context = ();
     type Offset = usize;
-    fn new(_context: (), range: std::ops::Range<usize>) -> Self {
-        Span(range.start(), range.end())
-    }
-    fn context(&self) -> () { () }
-    fn start(&self) -> usize { self.0 }
-    fn end(&self) -> usize { self.1 }
 }
 
-impl traits::sealed::SpanTracker for sealed::OffsetTracker {
-    type Span = Span;
-    fn next_span(&mut self, c: char) -> Span {
-        let start = self.offset;
-        self.offset += c.len_utf8();
-        Span(start, self.offset)
-    }
-}
+// #[allow(missing_debug_implementations)]
+// mod sealed {
+//     pub struct OffsetTracker {
+//         pub(crate) offset: usize,
+//     }
 
-impl traits::sealed::Sealed for Span {
-    type Tracker = sealed::OffsetTracker;
-    fn at_start(&self, chars: usize) -> Self {
-        Span(self.0, self.0+chars)
-    }
+//     #[cfg(feature = "line-numbers")]
+//     pub struct LineTracker {
+//         pub(crate) offset: usize,
+//         pub(crate) caret_return: bool,
+//         pub(crate) line: usize,
+//         pub(crate) column: usize,
+//     }
 
-    fn at_end(&self) -> Self {
-        Span(self.1, self.1)
-    }
+// }
 
-    fn before_start(&self, chars: usize) -> Self {
-        Span(self.0.saturating_sub(chars), self.0)
-    }
+// impl traits::sealed::SpanTracker for sealed::OffsetTracker {
+//     type Span = Span;
+//     fn next_span(&mut self, c: char) -> Span {
+//         let start = self.offset;
+//         self.offset += c.len_utf8();
+//         Span(start, self.offset)
+//     }
+// }
 
-    fn length(&self) -> usize {
-        self.1.saturating_sub(self.0)
-    }
+// impl traits::sealed::Sealed for Span {
+//     type Tracker = sealed::OffsetTracker;
+//     fn at_start(&self, chars: usize) -> Self {
+//         Span(self.0, self.0 + chars)
+//     }
 
-    fn stream(text: &str) -> traits::sealed::Stream<'_, Self, Self::Tracker>
-        where Self: chumsky::Span
-    {
-        chumsky::Stream::from_iter(
-            Span(text.len(), text.len()),
-            traits::sealed::Map(text.chars(),
-                                sealed::OffsetTracker { offset: 0 }),
-        )
-    }
-}
+//     fn at_end(&self) -> Self {
+//         Span(self.1, self.1)
+//     }
 
-impl traits::Span for Span {}
+//     fn before_start(&self, chars: usize) -> Self {
+//         Span(self.0.saturating_sub(chars), self.0)
+//     }
 
-impl chumsky::Span for LineSpan {
+//     fn len(&self) -> usize {
+//         self.1.saturating_sub(self.0)
+//     }
+
+//     // fn stream(text: &str) -> traits::sealed::Stream<'_, Self, Self::Tracker>
+//     //     where Self: chumsky::Span
+//     // {
+//     //     chumsky::Stream::from_iter(
+//     //         Span(text.len(), text.len()),
+//     //         traits::sealed::Map(text.chars(),
+//     //                             sealed::OffsetTracker { offset: 0 }),
+//     //     )
+//     // }
+// }
+
+impl chumsky::zero_copy::span::Span for LineSpan {
     type Context = ();
     type Offset = LinePos;
-    fn new(_context: (), range: std::ops::Range<LinePos>) -> Self {
-        LineSpan(range.start, range.end)
-    }
-    fn context(&self) -> () { () }
-    fn start(&self) -> LinePos { self.0 }
-    fn end(&self) -> LinePos { self.1 }
+    // fn new(_context: (), range: std::ops::Range<LinePos>) -> Self {
+    //     LineSpan(range.start, range.end)
+    // }
+    // fn context(&self) -> () { () }
+    // fn start(&self) -> LinePos { self.0 }
+    // fn end(&self) -> LinePos { self.1 }
 }
 
-#[cfg(feature = "line-numbers")]
-impl traits::sealed::SpanTracker for sealed::LineTracker {
-    type Span = LineSpan;
-    fn next_span(&mut self, c: char) -> LineSpan {
-        let offset = self.offset;
-        let line = self.line;
-        let column = self.column;
-        self.offset += c.len_utf8();
-        match c {
-            '\n' if self.caret_return => {}
-            '\r'|'\n'|'\x0C'|'\u{0085}'|'\u{2028}'|'\u{2029}' => {
-                self.line += 1;
-                self.column = 0;
-            }
-            '\t' => self.column += 8,
-            c => {
-                self.column += unicode_width::UnicodeWidthChar::width(c)
-                    .unwrap_or(0);  // treat control chars as zero-length
-            }
-        }
-        self.caret_return = c == '\r';
-        LineSpan(
-            LinePos {
-                line,
-                column,
-                offset,
-            },
-            LinePos {
-                line: self.line,
-                column: self.column,
-                offset: self.offset,
-            },
-        )
-    }
-}
+// #[cfg(feature = "line-numbers")]
+// impl traits::sealed::SpanTracker for sealed::LineTracker {
+//     type Span = LineSpan;
+//     fn next_span(&mut self, c: char) -> LineSpan {
+//         let offset = self.offset;
+//         let line = self.line;
+//         let column = self.column;
+//         self.offset += c.len_utf8();
+//         match c {
+//             '\n' if self.caret_return => {}
+//             '\r'|'\n'|'\x0C'|'\u{0085}'|'\u{2028}'|'\u{2029}' => {
+//                 self.line += 1;
+//                 self.column = 0;
+//             }
+//             '\t' => self.column += 8,
+//             c => {
+//                 self.column += unicode_width::UnicodeWidthChar::width(c)
+//                     .unwrap_or(0);  // treat control chars as zero-length
+//             }
+//         }
+//         self.caret_return = c == '\r';
+//         LineSpan(
+//             LinePos {
+//                 line,
+//                 column,
+//                 offset,
+//             },
+//             LinePos {
+//                 line: self.line,
+//                 column: self.column,
+//                 offset: self.offset,
+//             },
+//         )
+//     }
+// }
 
-#[cfg(feature = "line-numbers")]
-impl traits::sealed::Sealed for LineSpan {
-    type Tracker = sealed::LineTracker;
-    /// Note assuming ascii, single-width, non-newline chars here
-    fn at_start(&self, chars: usize) -> Self {
-        LineSpan(self.0, LinePos {
-            offset: self.0.offset + chars,
-            column: self.0.column + chars,
-            .. self.0
-        })
-    }
+// #[cfg(feature = "line-numbers")]
+// impl traits::sealed::Sealed for LineSpan {
+//     type Tracker = sealed::LineTracker;
+//     /// Note assuming ascii, single-width, non-newline chars here
+//     fn at_start(&self, chars: usize) -> Self {
+//         LineSpan(self.0, LinePos {
+//             offset: self.0.offset + chars,
+//             column: self.0.column + chars,
+//             .. self.0
+//         })
+//     }
 
-    fn at_end(&self) -> Self {
-        LineSpan(self.1, self.1)
-    }
+//     fn at_end(&self) -> Self {
+//         LineSpan(self.1, self.1)
+//     }
 
-    /// Note assuming ascii, single-width, non-newline chars here
-    fn before_start(&self, chars: usize) -> Self {
-        LineSpan(LinePos {
-            offset: self.0.offset.saturating_sub(chars),
-            column: self.0.column.saturating_sub(chars),
-            .. self.0
-        }, self.0)
-    }
+//     /// Note assuming ascii, single-width, non-newline chars here
+//     fn before_start(&self, chars: usize) -> Self {
+//         LineSpan(LinePos {
+//             offset: self.0.offset.saturating_sub(chars),
+//             column: self.0.column.saturating_sub(chars),
+//             .. self.0
+//         }, self.0)
+//     }
 
-    fn length(&self) -> usize {
-        self.1.offset.saturating_sub(self.0.offset)
-    }
+//     fn len(&self) -> usize {
+//         self.1.offset.saturating_sub(self.0.offset)
+//     }
 
-    fn stream(text: &str) -> traits::sealed::Stream<'_, Self, Self::Tracker>
-        where Self: chumsky::Span
-    {
-        let mut caret_return = false;
-        let mut line = 0;
-        let mut last_line = text;
-        let mut iter = text.chars();
-        while let Some(c) = iter.next() {
-            match c {
-                '\n' if caret_return => {}
-                '\r'|'\n'|'\x0C'|'\u{0085}'|'\u{2028}'|'\u{2029}' => {
-                    line += 1;
-                    last_line = iter.as_str();
-                }
-                _ => {}
-            }
-            caret_return = c == '\r';
-        }
-        let column = unicode_width::UnicodeWidthStr::width(last_line);
-        let eoi = LinePos {
-            line,
-            column,
-            offset: text.len(),
-        };
-        chumsky::Stream::from_iter(
-            LineSpan(eoi, eoi),
-            traits::sealed::Map(
-                text.chars(),
-                sealed::LineTracker {
-                    caret_return: false,
-                    offset: 0,
-                    line: 0,
-                    column: 0,
-                },
-            ),
-        )
-    }
-}
-
-#[cfg(feature = "line-numbers")]
-impl traits::Span for LineSpan {}
+//     // fn stream(text: &str) -> traits::sealed::Stream<'_, Self, Self::Tracker>
+//     //     where Self: chumsky::Span
+//     // {
+//     //     let mut caret_return = false;
+//     //     let mut line = 0;
+//     //     let mut last_line = text;
+//     //     let mut iter = text.chars();
+//     //     while let Some(c) = iter.next() {
+//     //         match c {
+//     //             '\n' if caret_return => {}
+//     //             '\r'|'\n'|'\x0C'|'\u{0085}'|'\u{2028}'|'\u{2029}' => {
+//     //                 line += 1;
+//     //                 last_line = iter.as_str();
+//     //             }
+//     //             _ => {}
+//     //         }
+//     //         caret_return = c == '\r';
+//     //     }
+//     //     let column = unicode_width::UnicodeWidthStr::width(last_line);
+//     //     let eoi = LinePos {
+//     //         line,
+//     //         column,
+//     //         offset: text.len(),
+//     //     };
+//     //     chumsky::Stream::from_iter(
+//     //         LineSpan(eoi, eoi),
+//     //         traits::sealed::Map(
+//     //             text.chars(),
+//     //             sealed::LineTracker {
+//     //                 caret_return: false,
+//     //                 offset: 0,
+//     //                 line: 0,
+//     //                 column: 0,
+//     //             },
+//     //         ),
+//     //     )
+//     // }
+// }
 
 impl Display for Span {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {

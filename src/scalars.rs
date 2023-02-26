378423829ea4bcb79fc306420e3007ad09772d7a
root <rnarkkx@gmail.com>
2023-02-26 09:50:57 +0000

    Update

--- src/scalars.rs
+++ src/scalars.rs
@@ -16,7 +16,7 @@ type I<'a> = &'a str;
 type Extra = Full<ParseError, Context, ()>;
 
 use crate::{
-    ast::{Scalar, Integer, Decimal},
+    ast::Scalar,
     context::Context,
     errors::{DecodeError, ExpectedType, EncodeError, ParseError},
     traits::{DecodeScalar, EncodeScalar}
@@ -30,7 +30,7 @@ fn digits<'a>(radix: u32) -> impl Parser<'a, I<'a>, &'a str, Extra> {
     any().filter(move |c: &char| c == &'_' || c.is_digit(radix)).repeated().map_slice(|x| x)
 }
 
-fn decimal_number<'a>() -> impl Parser<'a, I<'a>, Box<str>, Extra> {
+fn decimal_number<'a, O>() -> impl Parser<'a, I<'a>, O, Extra> {
     just('-').or(just('+')).or_not()
     .then(digit(10)).then(digits(10))
     .then(just('.').then(digit(10)).then(digits(10)).or_not())
@@ -38,17 +38,11 @@ fn decimal_number<'a>() -> impl Parser<'a, I<'a>, Box<str>, Extra> {
            .then(just('-').or(just('+')).or_not())
            .then(digits(10)).or_not())
     .map_slice(|v| {
-        // let is_decimal = v.chars().any(|c| matches!(c, '.'|'e'|'E'));
         v.chars().filter(|c| c != &'_').collect::<String>().into()
-        // if is_decimal {
-        //     Decimal(s.into())
-        // } else {
-        //     Int(Integer(10, s.into())
-        // }
     })
 }
 
-fn radix_number<'a>() -> impl Parser<'a, I<'a>, Box<str>, Extra> {
+fn radix_number<'a, O>() -> impl Parser<'a, I<'a>, O, Extra> {
     // sign
     just('-').or(just('+')).or_not()
     .then_ignore(just('0'))
@@ -64,28 +58,16 @@ fn radix_number<'a>() -> impl Parser<'a, I<'a>, Box<str>, Extra> {
         let mut s = String::with_capacity(value.len() + sign.map_or(0, |_| 1));
         sign.map(|c| s.push(c));
         s.extend(value.chars().filter(|&c| c != '_'));
-        Integer(radix, s.into())
+        O::from_str_radix(s.into(), radix)
     })
 }
 
-fn number<'a>() -> impl Parser<'a, I<'a>, Box<str>, Extra> {
+fn number<'a, O>() -> impl Parser<'a, I<'a>, O, Extra> {
     radix_number().or(decimal_number())
 }
 
 macro_rules! impl_integer {
-    ($ty:ident, $marker:ident) => {
-        impl TryFrom<&Integer> for $ty {
-            type Error = <$ty as FromStr>::Err;
-            fn try_from(val: &Integer) -> Result<$ty, <$ty as FromStr>::Err> {
-                match val.0 {
-                    2 => <$ty>::from_str_radix(&val.1, 2),
-                    8 => <$ty>::from_str_radix(&val.1, 8),
-                    10 => <$ty>::from_str(&val.1),
-                    16 => <$ty>::from_str_radix(&val.1, 16),
-                }
-            }
-        }
-
+    ($ty:ident) => {
         impl DecodeScalar for $ty {
             fn decode(scalar: &Scalar, ctx: &mut Context)
                 -> Result<Self, DecodeError>
@@ -100,14 +82,9 @@ macro_rules! impl_integer {
                         });
                     }
                 }
-                scalar.literal.as_ref().try_into().map_err(|err| DecodeError::conversion(ctx.span(&scalar), err))
-            }
-        }
-
-        impl TryFrom<&$ty> for Integer {
-            type Error = <$ty as FromStr>::Err;
-            fn try_from(val: &$ty) -> Result<Integer, <$ty as FromStr>::Err> {
-                Ok(Integer(10, val.to_string().into()))
+                number().parse_with_state(scalar.literal.as_ref(), ctx)
+                    .into_result()
+                    .map_err(|err| DecodeError::conversion(ctx.span(&scalar), err))
             }
         }
 
@@ -120,35 +97,19 @@ macro_rules! impl_integer {
     }
 }
 
-impl_integer!(i8, I8);
-impl_integer!(u8, U8);
-impl_integer!(i16, I16);
-impl_integer!(u16, U16);
-impl_integer!(i32, I32);
-impl_integer!(u32, U32);
-impl_integer!(i64, I64);
-impl_integer!(u64, U64);
-impl_integer!(isize, Isize);
-impl_integer!(usize, Usize);
+impl_integer!(i8);
+impl_integer!(u8);
+impl_integer!(i16);
+impl_integer!(u16);
+impl_integer!(i32);
+impl_integer!(u32);
+impl_integer!(i64);
+impl_integer!(u64);
+impl_integer!(isize);
+impl_integer!(usize);
 
 macro_rules! impl_decimal {
-    ($ty:ident, $marker:ident) => {
-        impl TryFrom<&Integer> for $ty {
-            type Error = <$ty as FromStr>::Err;
-            fn try_from(val: &Integer) -> Result<$ty, <$ty as FromStr>::Err>
-            {
-                <$ty>::from_str(&val.1)
-            }
-        }
-
-        impl TryFrom<&Decimal> for $ty {
-            type Error = <$ty as FromStr>::Err;
-            fn try_from(val: &Decimal) -> Result<$ty, <$ty as FromStr>::Err>
-            {
-                <$ty>::from_str(&val.0)
-            }
-        }
-
+    ($ty:ident) => {
         impl DecodeScalar for $ty {
             fn decode(scalar: &Scalar, ctx: &mut Context)
                 -> Result<Self, DecodeError>
@@ -163,18 +124,11 @@ macro_rules! impl_decimal {
                         });
                     }
                 }
-                scalar.literal.try_into().map_err(|err| DecodeError::conversion(
+                <$ty>::from_str(scalar.literal.as_ref()).map_err(|err| DecodeError::conversion(
                                  ctx.span(&scalar), err))
             }
         }
 
-        impl TryFrom<&$ty> for Decimal {
-            type Error = <$ty as FromStr>::Err;
-            fn try_from(val: &$ty) -> Result<Decimal, <$ty as FromStr>::Err> {
-                Ok(Decimal(val.to_string().into()))
-            }
-        }
-
         impl EncodeScalar for $ty {
             fn encode(&self, _: &mut Context) -> Result<Scalar, EncodeError> {
                 let literal = format!("{}", self);
@@ -184,8 +138,8 @@ macro_rules! impl_decimal {
     }
 }
 
-impl_decimal!(f32, F32);
-impl_decimal!(f64, F64);
+impl_decimal!(f32);
+impl_decimal!(f64);
 
 impl DecodeScalar for String {
     fn decode(scalar: &Scalar, ctx: &mut Context) -> Result<Self, DecodeError> {

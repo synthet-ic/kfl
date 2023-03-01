ef649899dba5fc95229d0ef2a82eb3772502dac9
Rin Arakaki <rnarkkx@gmail.com>
2023-03-01 14:04:14 +0900

    Update consts.rs

--- repr/src/consts.rs
+++ repr/src/consts.rs
@@ -1,7 +1,13 @@
 use std::sync::LazyLock;
 use crate::pat::Pat;
 
-pub const CR: LazyLock<Pat> = LazyLock::new(|| Pat::from('\r'));
+/// `\t`
+pub const TAB: LazyLock<Pat> = LazyLock::new(|| u!(0009));
+/// `\n`
+pub const LF: LazyLock<Pat> = LazyLock::new(|| u!(000A));
+/// `\r`
+pub const CR: LazyLock<Pat> = LazyLock::new(|| u!(000D));
+
 pub const EMPTY: LazyLock<Pat> = LazyLock::new(|| Pat::empty());
 pub const DIGIT: LazyLock<Pat> = LazyLock::new(|| Pat::empty() & ('0'..'9'));
 pub const SPACE: LazyLock<Pat> = LazyLock::new(|| Pat::empty() & ' ');

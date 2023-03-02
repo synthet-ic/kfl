54219486a53cba98d93a122923ce40a594a9db64
Rin Arakaki <rnarkkx@gmail.com>
2023-03-02 22:39:30 +0900

    Update repr.rs

--- repr/src/repr.rs
+++ repr/src/repr.rs
@@ -1,10 +1,12 @@
+use alloc::boxed::Box;
+
 pub enum Repr<I> {
     Empty,
-    Range(I),
-    Not(Repr<I>),
-    Or(Repr<I>, Repr<I>),
-    And(Repr<I>, Repr<I>),
-    Xor(Repr<I>, Repr<I>),
-    Add(Repr<I>, I),
-    Sub(Repr<I>, I)
+    Range(I),  // Or(Empty, I)?
+    Not(Box<Repr<I>>),
+    Or(Box<Repr<I>>, Box<Repr<I>>),
+    And(Box<Repr<I>>, Box<Repr<I>>),
+    Xor(Box<Repr<I>>, Box<Repr<I>>),
+    Add(Box<Repr<I>>, I),
+    Sub(Box<Repr<I>>, I)
 }

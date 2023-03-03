ce0182ab8fa0721485d22de2be0624f218f04a78
root <rnarkkx@gmail.com>
2023-03-03 05:25:03 +0000

    Update

--- repr/src/convert.rs
+++ repr/src/convert.rs
@@ -1,8 +1,8 @@
 use core::ops::{
-    Range, RangeFrom, RangeTo, RangeFull, RangeBounds, Bound
+    self, RangeFrom, RangeTo, RangeFull, RangeBounds, Bound
 };
 
-use crate::repr::Repr;
+use crate::repr::{Repr, Seq, Range};
 
 impl From<char> for Repr<char> {
     fn from(value: char) -> Self {
@@ -16,14 +16,8 @@ impl From<&str> for Repr<char> {
     }
 }
 
-impl From<&Hir> for Repr<char> {
-    fn from(value: &Hir) -> Self {
-        Self(value.clone())
-    }
-}
-
-impl From<Range<char>> for Repr<char> {
-    fn from(value: Range<char>) -> Self {
+impl From<ops::Range<char>> for Repr<char> {
+    fn from(value: ops::Range<char>) -> Self {
         Self::range(value)
     }
 }
@@ -34,13 +28,18 @@ impl<T: Into<Repr<char>>> From<[T; 1]> for Repr<char> {
     }
 }
 
-// impl Deref for Repr<char> {
-//     type Target = *mut Self;
-
-//     fn deref(&self) -> &Self::Target {
-//         &(*self * ..)
-//     }
-// }
+impl const From<R: RangeBounds<usize>> for Range {
+    fn from(range: R) -> Self {
+        match (range.start_bound(), range.end_bound()) {
+            (Bound::Unbounded, Bound::Unbounded) => Range::Empty,
+            (Bound::Unbounded, Bound::Excluded(end)) => Range::To(end),
+            (Bound::Included(start), Bound::Unbounded) => Range::From(start),
+            (Bound::Included(start), Bound::Excluded(end))
+                => Range::Full(start, end),
+            _ => panic!("Try m..n in place of m..=n.")
+        }
+    }
+}
 
 impl Into<Regex> for Repr<char> {
     fn into(self) -> Regex {

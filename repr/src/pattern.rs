00c24a46e4f4bc1796abf618cebc5e4b518ae37a
root <rnarkkx@gmail.com>
2023-03-03 05:57:52 +0000

    Update

--- repr/src/pattern.rs
+++ repr/src/pattern.rs
@@ -1,65 +1,11 @@
-use core::{
-    str::pattern::{Pattern, Searcher, SearchStep}
-};
+use core::str::pattern::{Pattern, Searcher, SearchStep};
 use regex::{Regex, Matches};
 use regex_syntax::{
     Parser,
-    hir::{
-        Hir, HirKind, Repetition, RepetitionKind, RepetitionRange, Class,
-        ClassUnicode, ClassUnicodeRange,
-        print::Printer
-    }
+    hir::print::Printer
 };
 
-#[derive(Clone, Debug, PartialEq)]
-pub struct Pat(Hir);
-
-impl Pat {
-    pub fn new(re: &str) -> Self {
-        let hir = Parser::new().parse(re).unwrap();
-        Self(hir)
-    }
-
-    pub fn empty() -> Self {
-        Self(Hir::empty())
-    }
-
-    pub fn or<P: Into<Self>>(self, pat: P) -> Self {
-        let hir = Hir::alternation(vec![self.0, pat.into().0]);
-        Self(hir)
-    }
-
-    pub fn and<P: Into<Self>>(self, pat: P) -> Self {
-        let hir = Hir::concat(vec![self.0, pat.into().0]);
-        Self(hir)
-    }
-
-    pub fn range(range: Range<char>) -> Self {
-        let range = ClassUnicodeRange::new(range.start, range.end);
-        let class = ClassUnicode::new([range]);
-        Self(Hir::class(Class::Unicode(class)))
-    }
-
-    pub fn mul<R: RangeBounds<u32>>(self, range: R) -> Self {
-        match (range.start_bound(), range.end_bound()) {
-            (Bound::Unbounded, Bound::Unbounded) => {
-                self * ..
-            },
-            (Bound::Unbounded, Bound::Excluded(end)) => {
-                self * (..*end)
-            },
-            (Bound::Included(start), Bound::Unbounded) => {
-                self * (*start..)
-            },
-            (Bound::Included(start), Bound::Excluded(end)) => {
-                self * (*start..*end)
-            },
-            _ => {
-                panic!("In regex-ext, m..n is interpreted as m..=n.");
-            }
-        }
-    }
-}
+use crate::repr::Repr;
 
 // #[derive(Debug)]
 // pub struct RegexSearcher<'r, 't> {
@@ -69,7 +15,7 @@ impl Pat {
 //     next_match: Option<(usize, usize)>,
 // }
 
-// impl<'r, 't> Pattern<'t> for &'r Pat {
+// impl<'r, 't> Pattern<'t> for &'r Repr {
 //     type Searcher = RegexSearcher<'r, 't>;
 
 //     fn into_searcher(self, haystack: &'t str) -> RegexSearcher<'r, 't> {

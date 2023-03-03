2ce8a1653e83f2d4d419a04b5331b6b12f8b020a
Rin Arakaki <rnarkkx@gmail.com>
2023-03-03 23:51:02 +0900

    Update repr.rs

--- repr/src/repr.rs
+++ repr/src/repr.rs
@@ -19,7 +19,7 @@ pub enum Repr<I: ~const Integral> {
     Or(Box<Repr<I>>, Box<Repr<I>>),
     And(Box<Repr<I>>, Box<Repr<I>>),
     Xor(Box<Repr<I>>, Box<Repr<I>>),
-    Add(Box<Repr<I>>, Box<Repr<I>>),
+    Add(Box<Repr<I>>, Box<Repr<I>>),  // RegexSet
     Sub(Box<Repr<I>>, Seq<I>),
     Mul(Box<Repr<I>>, Range),
     // Map(Box<Repr<I>>, Fn(Box<Repr<I>>), Fn(Box<Repr<I>>))

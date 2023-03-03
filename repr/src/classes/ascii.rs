3b3438a4e560c8e7a28077c42f1a2e4c31ccb866
Rin Arakaki <rnarkkx@gmail.com>
2023-03-03 18:21:14 +0900

    Update ascii.rs

--- repr/src/classes/ascii.rs
+++ repr/src/classes/ascii.rs
@@ -60,3 +60,37 @@ impl ClassAsciiKind {
         }
     }
 }
+
+fn fmt_class_ascii(&mut self, ast: &ast::ClassAscii) -> fmt::Result {
+    use crate::ast::ClassAsciiKind::*;
+    match ast.kind {
+        Alnum if ast.negated => self.wtr.write_str("[:^alnum:]"),
+        Alnum => self.wtr.write_str("[:alnum:]"),
+        Alpha if ast.negated => self.wtr.write_str("[:^alpha:]"),
+        Alpha => self.wtr.write_str("[:alpha:]"),
+        Ascii if ast.negated => self.wtr.write_str("[:^ascii:]"),
+        Ascii => self.wtr.write_str("[:ascii:]"),
+        Blank if ast.negated => self.wtr.write_str("[:^blank:]"),
+        Blank => self.wtr.write_str("[:blank:]"),
+        Cntrl if ast.negated => self.wtr.write_str("[:^cntrl:]"),
+        Cntrl => self.wtr.write_str("[:cntrl:]"),
+        Digit if ast.negated => self.wtr.write_str("[:^digit:]"),
+        Digit => self.wtr.write_str("[:digit:]"),
+        Graph if ast.negated => self.wtr.write_str("[:^graph:]"),
+        Graph => self.wtr.write_str("[:graph:]"),
+        Lower if ast.negated => self.wtr.write_str("[:^lower:]"),
+        Lower => self.wtr.write_str("[:lower:]"),
+        Print if ast.negated => self.wtr.write_str("[:^print:]"),
+        Print => self.wtr.write_str("[:print:]"),
+        Punct if ast.negated => self.wtr.write_str("[:^punct:]"),
+        Punct => self.wtr.write_str("[:punct:]"),
+        Space if ast.negated => self.wtr.write_str("[:^space:]"),
+        Space => self.wtr.write_str("[:space:]"),
+        Upper if ast.negated => self.wtr.write_str("[:^upper:]"),
+        Upper => self.wtr.write_str("[:upper:]"),
+        Word if ast.negated => self.wtr.write_str("[:^word:]"),
+        Word => self.wtr.write_str("[:word:]"),
+        Xdigit if ast.negated => self.wtr.write_str("[:^xdigit:]"),
+        Xdigit => self.wtr.write_str("[:xdigit:]"),
+    }
+}

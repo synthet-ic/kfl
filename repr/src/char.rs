55d0da9b9d5539c7547a4c762627c5d161b73810
root <rnarkkx@gmail.com>
2023-03-03 05:54:57 +0000

    Update

--- repr/src/char.rs
+++ repr/src/char.rs
@@ -1,9 +1,3 @@
-// use core::{
-//     ops::{
-//         BitOr, BitAnd, Range, RangeFrom, RangeTo, Mul, RangeBounds, Bound, Try,
-//         ControlFlow, FromResidual, RangeFull
-//     },
-// };
 use crate::repr::Repr;
 
 pub trait CharExt: Into<Repr<char>> {
@@ -17,19 +11,3 @@ pub trait CharExt: Into<Repr<char>> {
 }
 
 impl CharExt for char {}
-
-// impl<T: CharExt> BitAnd<T> for T {
-//     type Output = Repr<char>;
-
-//     fn bitand(self, rhs: T) -> Self::Output {
-//         self.and(rhs)
-//     }
-// }
-
-// impl<T: CharExt> BitOr<T> for T {
-//     type Output = Repr<char>;
-    
-//     fn bitor(self, rhs: T) -> Self::Output {
-//         self.or(rhs)
-//     }
-// }

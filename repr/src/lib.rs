e1db1c12544f562be8c1cbd0956dc3a6a088aa63
Rin Arakaki <rnarkkx@gmail.com>
2023-03-04 09:49:01 +0900

    Update lib.rs

--- repr/src/lib.rs
+++ repr/src/lib.rs
@@ -2,22 +2,22 @@
 # Usage
 
 ```text
-use repr::{DIGIT, Pattern};
-let re = DIGIT * 4 & "-" & DIGIT * 2 & "-" & DIGIT * 2;
+use repr::{Repr, consts::DIGIT};
+let re = DIGIT * 4 & '-' & DIGIT * 2 & '-' & DIGIT * 2;
 assert!(re.is_match("2014-01-01"));
 ```
 
 ```text
-let re = (D * 4)["year"] & "-" & (D * 2)["month"] & "-" & (D * 2)["day"];
+let re = (D * 4)["year"] & '-' & (D * 2)["month"] & '-' & (D * 2)["day"];
 let before = "2012-03-14, 2013-01-01 and 2014-07-05";
 let after = re.replace_all(before, "$m/$d/$y");
 assert_eq!(after, "03/14/2012, 01/01/2013 and 07/05/2014");
 ```
 
 ```text
-use repr::{WORD, Pattern};
-let wh = WORD | "-";
-let re = (wh | ".") * 1.. & "@" & (wh * 1.. & ".") * 1.. & wh * 2..4;
+use repr::{Repr, consts::DIGIT};
+let wh = WORD | '-';
+let re = (wh | '.') * 1.. & '@' & (wh * 1.. & '.') * 1.. & wh * 2..4;
 ```
 */
 
@@ -49,7 +49,7 @@ pub use repr::{Repr, Seq, Range};
 //     let year = DIGIT * 4;
 //     let month = DIGIT * 2;
 //     let day = DIGIT * 2;
-//     let re = year & "-" & month & "-" & day;
+//     let re = year & '-' & month & '-' & day;
 //     let before = "2012-03-14, 2013-01-01 and 2014-07-05";
 //     let after = re.replace_all(before, "$m/$d/$y");
 //     assert_eq!(after, "03/14/2012, 01/01/2013 and 07/05/2014");

6fcc274bca3bec79b60bd1f86c879845aebd4339
root <rnarkkx@gmail.com>
2023-03-06 12:29:40 +0000

    Update

--- unconst/src/lib.rs
+++ unconst/src/lib.rs
@@ -1,31 +1,43 @@
-#![no_std]
+// #![no_std]
 
 use core::clone::Clone;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse, ItemFn, ItemTrait, TraitItem};
+use syn::{parse, ItemFn, ItemTrait, TraitItem, ItemImpl, ImplItem};
 
 #[proc_macro_attribute]
 pub fn unconst(_attr: TokenStream, item: TokenStream) -> TokenStream {
     match parse::<ItemFn>(item.clone()) {
-        Ok(mut item) => {
-            item.sig.constness = None;
-            return quote!(item).into()
+        Ok(mut r#fn) => {
+            r#fn.sig.constness = None;
+            return quote!(#r#fn).into();
         }
         Err(_) => {}
     };
-    match parse::<ItemTrait>(item) {
-        Ok(mut item) => {
-            for item in item.items.iter_mut() {
+    match parse::<ItemTrait>(item.clone()) {
+        Ok(mut r#trait) => {
+            for item in r#trait.items.iter_mut() {
                 match item {
                     TraitItem::Method(method) => method.sig.constness = None,
                     _ => continue
                 };
             }
-            return quote!(item).into()
+            return quote!(#r#trait).into()
         }
         Err(_) => {}
     };
-    panic!("Input is neither a function nor a trait")
+    match parse::<ItemImpl>(item.clone()) {
+        Ok(mut r#impl) => {
+            for item in r#impl.items.iter_mut() {
+                match item {
+                    ImplItem::Method(method) => method.sig.constness = None,
+                    _ => continue
+                };
+            }
+            return quote!(#r#impl).into()
+        }
+        Err(_) => {}
+    };
+    panic!("Input is neither a function, a trait nor an impl");
 }
